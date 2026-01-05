2e37a3adb2691f11f011a4e69c895c0ce6c4773c
Manthan Jasoliya <manthanjasoliya84014@gmail.com>
2026-01-05 22:06:07 +0530

    Navbar added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,11 @@
-import './App.css'
+import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home.jsx'
+import Home from './pages/Home.jsx';
+import Navbar from './components/Navbar.jsx';
 function App() {
   return (
     <div>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
       </Routes>
