f344ef145aa9c42e22cdc36477bf57dac1b40880
Manthan Jasoliya <manthanjasoliya84014@gmail.com>
2026-01-05 23:51:11 +0530

    Login and Register pages added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
+import Login from './pages/Login.jsx';
+import Register from './pages/Register.jsx';
 import Navbar from './components/Navbar.jsx';
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
       </Routes>
     </div>
   )
---
+++ Frontend/src/pages/Register.jsx
@@ -0,0 +1,8 @@
+function Register(){
+    return (
+        <div>
+            <h2>Register Page</h2>
+        </div>
+    );
+}
+export default Register;
