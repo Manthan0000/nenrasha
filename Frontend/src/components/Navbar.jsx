7240ad6e35d1e23f306e2174ea36ea14b6bccc39
Manthan Jasoliya <manthanjasoliya84014@gmail.com>
2026-01-06 22:41:29 +0530

    Simple navbar removed and MUI AppBar added

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,8 +1,20 @@
+import {AppBar, Toolbar, Typography, Button, Box} from '@mui/material';
+import {Link} from 'react-router-dom';
+
 function Navbar(){
     return (
-        <nav>
-            <h2>Nenrasha</h2>
-        </nav>
+        <AppBar position="static" color="transparent" elevation={0}>
+            <Toolbar sx={{justifyContent: 'space-between'}}>
+                {/* Logo */}
+                <Typography variant="h6" component={Link} to="/" sx={{textDecoration: 'none', color: 'black', fontweight: 'bold'}}>Nenrasha</Typography>
+                {/*Menu*/}
+                <Box>
+                    <Button component={Link} to="/" sx={{color: 'black', textTransform: 'none'}}>Home</Button>
+                    <Button component={Link} to="/login" sx={{color: 'black', textTransform: 'none'}}>Login</Button>
+                    <Button component={Link} to="/register" sx={{color: 'black', textTransform: 'none'}}>Register</Button>
+                </Box>
+            </Toolbar>
+        </AppBar>
     );
 }
 export default Navbar;
