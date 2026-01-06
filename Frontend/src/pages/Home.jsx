b1c1fad1077c7dec91d4d1e22ed298ed1ac09230
Manthan Jasoliya <manthanjasoliya84014@gmail.com>
2026-01-06 22:54:24 +0530

    basic Hero section added

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,8 +1,22 @@
+import { Box, Typography, Button } from "@mui/material";
+
 function Home(){
     return (
-        <div>
-            <h2>Welcome to Nenrasha</h2>
-        </div>
+        <Box sx={{minHeight: '80vh', display: 'flex', alignItems: 'center', justifyContent: 'center',textAlign: 'center',
+            px: 2,
+        }}>
+            <Box>
+                <Typography variant="h3" fontWeight="bold" gutterBottom>
+                    Discover Your Style
+                </Typography>
+                <Typography variant="subtitle1" color="text.secondary" gutterBottom>
+                    Explore the latest fashion trends at Nenrasha
+                </Typography>
+                <Button variant="contained" size="large">
+                    Shop Now
+                </Button>
+            </Box>
+        </Box>
     );
 }
 export default Home;
