5dc8a8d585ce6866514ab4cde17b7d9b8a1e0c44
Manthan Jasoliya <manthanjasoliya84014@gmail.com>
2026-01-08 19:05:02 +0530

    User Schema added

---
+++ Backend/src/models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+    {
+        name:{
+            type: String,
+            require: true,
+            trim: true,
+        },
+        email:{
+            type: String,
+            require: true,
+            unique: true,
+            lowercase: true,
+        },
+        password:{
+            type: String,
+            require: true,
+            minlength: 6,
+            select: false,
+        },
+        role:{
+            type: String,
+            enum: ['user', 'admin'],
+            default: 'user',
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+module.exports = mongoose.model('User', userSchema);
