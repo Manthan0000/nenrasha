4ce27a53203eff2e7952f903b2966cd2e80c0015
Manthan Jasoliya <manthanjasoliya84014@gmail.com>
2026-01-09 23:49:42 +0530

    Login and Register pages UI modified

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,8 +1,27 @@
+import {Box, Typography, TextField, Button} from '@mui/material';
+
 function Login(){
     return (
-        <div>
-            <h2>Login Page</h2>
-        </div>
+        <Box sx={{
+            minHeight: '80vh',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+        }}>
+            <Box sx={{
+                width: 360,
+                p: 4,
+                boxShadow: 3,
+                borderRadius: 2,
+            }}>
+                <Typography variant="h5" fontWeight="bold" gutterBottom>
+                    Login to Nenrasha
+                </Typography>
+                <TextField lable="Email" placeholder="Enter Email" type="email" fullWidth margin="normal" />
+                <TextField lable="Password" placeholder="Enter Password" type="Password" fullWidth margin="normal" />
+                <Button variant='contained' fullWidth sx={{ mt:2}}>Login</Button>
+            </Box>
+        </Box>
     );
 }
 export default Login;
--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -1,8 +1,28 @@
+import {Box ,Typography, TextField, Button} from '@mui/material'
+
 function Register(){
     return (
-        <div>
-            <h2>Register Page</h2>
-        </div>
+        <Box sx={{
+            minHeight: '80vh',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center'
+        }}>
+            <Box sx={{
+                width: 360,
+                p: 4,
+                boxShadow: 3,
+                borderRadius: 2,
+            }}>
+                <Typography variant='h5' fontWeight="bold" gutterBottom>
+                    Create Your account
+                </Typography>
+                <TextField label="Full Name" placeholder="Enter Your Name" fullWidth margin="normal"></TextField>
+                <TextField label="Email" placeholder="Enter Email Address" fullWidth margin="normal"></TextField>
+                <TextField label="Password" placeholder="Enter Password" fullWidth margin="normal"></TextField>
+                <Button variant='contained' fullWidth sx={{ mt: 2}}>Register</Button>
+            </Box>
+        </Box>
     );
 }
 export default Register;
