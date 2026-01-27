76a04cd73ce53e65844ce6c09c0dbb3ec15717df
Manthan Jasoliya <manthanjasoliya84014@gmail.com>
2026-01-27 14:32:10 +0530

    AuthController Related Routes added

--- Backend/src/routes/authRoutes.js
+++ Backend/src/routes/authRoutes.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const { registerUser, loginUser } = require('../controllers/authController');
 
-router.post('/register', (req,res) => {
-    res.send('Register Route');
-});
-
-router.post('/login', (req,res) => {
-    res.send('Login Route');
-});
+router.post('/register', registerUser);
+router.post('/login', loginUser);
 
 module.exports = router;
