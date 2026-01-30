d6cc6e04ed81c3ff4c881239c88e4257c39997ab
Manthan Jasoliya <manthanjasoliya84014@gmail.com>
2026-01-31 02:21:59 +0530

    cookie related dependancy added and app.js and app.jsx updated after cookie and session implemented

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,10 +2,16 @@ const express = require('express');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 
+const cookieParser = require('cookie-parser');
+
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173', // Vite default port
+    credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 
 app.get('/',(req,res) => {
