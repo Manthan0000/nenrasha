3f81118f9ee46d9fdeec702725712735ae3ea64e
Manthan Jasoliya <manthanjasoliya84014@gmail.com>
2026-02-01 21:53:04 +0530

    Home page updated ,and category tab logic remover and made clean

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,8 +1,10 @@
+import { useState } from 'react';
 import Categories from '../components/Categories.jsx';
 import HeroSection from "../components/HeroSection.jsx";
 import ProductTabs from '../components/ProductTabs.jsx';
 import ScrollToTop from "../components/ScrollToTop.jsx";
 import Footer from "../components/Footer.jsx";
+
 
 function Home(){
     return (
