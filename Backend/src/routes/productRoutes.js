737cbd1c7f4bd6d85ec2e7c0180fb98da1fdcdcb
Manthan Jasoliya <manthanjasoliya84014@gmail.com>
2026-02-11 00:09:21 +0530

    Product as per id open routes added

---
+++ Backend/src/routes/productRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const { getProducts, getProductById, createProduct } = require('../controllers/productController');
+const { protect, admin } = require('../middleware/authMiddleware');
+
+const upload = require('../middleware/uploadMiddleware');
+
+router.route('/')
+    .get(getProducts)
+    .post(protect, admin, upload.single('image'), createProduct);
+
+router.route('/:id')
+    .get(getProductById);
+
+router.route('/:id/visit')
+    .put(require('../controllers/productController').incrementVisits);
+
+router.route('/:id/like')
+    .put(protect, require('../controllers/productController').toggleLike);
+
+module.exports = router;
