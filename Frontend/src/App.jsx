fde88c2f5d20407fa1ec3ca15a1c68d1d8cd2403
Manthan Jasoliya <manthanjasoliya84014@gmail.com>
2026-03-09 00:48:14 +0530

    app.js and app.jsx updated for the cart and order intigration after test page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -12,32 +12,56 @@ import AddProduct from './pages/AddProduct.jsx';
 import MyListings from './pages/MyListings.jsx';
 import Products from './pages/Products.jsx';
 import LikedProducts from './pages/LikedProducts.jsx';
+import TermsAndConditions from './pages/TermsAndConditions.jsx';
+import PrivacyPolicy from './pages/PrivacyPolicy.jsx';
+import ReturnRefund from './pages/ReturnRefund.jsx';
+import ErrorPage from './pages/ErrorPage.jsx';
 
 import Navbar from './components/Navbar.jsx';
 import TopBar from './components/TopBar.jsx';
 import { AuthProvider } from './context/AuthContext';
+import { CartProvider } from './context/CartContext';
+import { DialogProvider } from './context/DialogContext';
+import Cart from './pages/Cart.jsx';
+import Checkout from './pages/Checkout.jsx';
+import MyOrders from './pages/MyOrders.jsx';
+import OrderDetails from './pages/OrderDetails.jsx';
 
 function App() {
   return (
     <AuthProvider>
-      <TopBar />
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/about' element={<About />}/>
-        <Route path='/contact' element={<Contact />}/>
-        <Route path='/blog' element={<Blog />}/>
-        <Route path='/my-account' element={<MyAccount />}/>
-        <Route path='/category/:categoryName' element={<CategoryPage />} />
-        <Route path='/product/:id' element={<ProductDetails />} />
-        <Route path='/products' element={<Products />} />
-        <Route path='/liked-products' element={<LikedProducts />} />
-        <Route path='/admin/add-product' element={<AddProduct />} />
-        <Route path='/admin/edit-product/:id' element={<AddProduct />} />
-        <Route path='/admin/my-listings' element={<MyListings />} />
-      </Routes>
+      <CartProvider>
+        <DialogProvider>
+          <TopBar />
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/about' element={<About />}/>
+            <Route path='/contact' element={<Contact />}/>
+            <Route path='/blog' element={<Blog />}/>
+            <Route path='/my-account' element={<MyAccount />}/>
+            <Route path='/category/:categoryName' element={<CategoryPage />} />
+            <Route path='/product/:id' element={<ProductDetails />} />
+            <Route path='/products' element={<Products />} />
+            <Route path='/liked-products' element={<LikedProducts />} />
+            <Route path='/admin/add-product' element={<AddProduct />} />
+            <Route path='/admin/edit-product/:id' element={<AddProduct />} />
+            <Route path='/admin/my-listings' element={<MyListings />} />
+            <Route path='/terms-conditions' element={<TermsAndConditions />} />
+            <Route path='/privacy-policy' element={<PrivacyPolicy />} />
+            <Route path='/return-refund' element={<ReturnRefund />} />
+            <Route path='/cart' element={<Cart />} />
+            <Route path='/checkout' element={<Checkout />} />
+            <Route path='/my-orders' element={<MyOrders />} />
+            <Route path='/my-orders/:id' element={<OrderDetails />} />
+            <Route path='/error' element={<ErrorPage />} />
+            {/* Catch-all: 404 for any unknown URL */}
+            <Route path='*' element={<ErrorPage code={404} />} />
+          </Routes>
+        </DialogProvider>
+      </CartProvider>
     </AuthProvider>
   )
 }
