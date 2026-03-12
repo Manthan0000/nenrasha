54133c08cf222b3591e362b7cf7afb0eb4b4b6ce
Manthan Jasoliya <manthanjasoliya84014@gmail.com>
2026-03-12 15:22:41 +0530

    Mail resend SPF & DKIN solved by Jugad

--- Backend/src/config/mail.js
+++ Backend/src/config/mail.js
@@ -1,10 +1,11 @@
 const { Resend } = require('resend');
 const resend = new Resend(process.env.RESEND_API_KEY);
+require('dotenv').config();
 
 exports.sendOTPEmail = async(email, otp) => {
     await resend.emails.send({
         from: 'onboarding@resend.dev',
-        to: email,
+        to: process.env.EMAIL,
         subject: 'Nenrasha Password Reset OTP',
         html: `<p>Your OTP for password reset is: <strong>${otp}</strong></p>`
     });
