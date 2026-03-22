b0d2ad32889d71fef27495fb4bb75af6d649e95e
Manthan Jasoliya <manthanjasoliya84014@gmail.com>
2026-03-23 00:48:59 +0530

    Vercel url in cors added

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,7 +8,8 @@ const app = express();
 
 app.use(cors({
     origin: ['http://localhost:5173', 'http://127.0.0.1:5173', // Vite dev server 
-        'http://localhost:3000','http://127.0.0.1:3000' // Docker dev server 
+        'http://localhost:3000','http://127.0.0.1:3000',// Docker dev server 
+        'https://nenrasha.vercel.app'
     ], // Allow both localhost and IP
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
